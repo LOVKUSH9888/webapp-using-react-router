4bd653369faa51cc492e0e0e09bb9fb159993980
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-16 22:48:44 +0530

    Adding all the pages and Routers

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import {Link, NavLink} from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import {Link, NavLink} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
